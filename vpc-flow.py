e782059c7809791b14bda6319ca4c8e3ebb16df4
starseed777 <waress66@live.com>
2021-02-21 13:48:23 -0500

    lambda

---
+++ vpc-flow.py
@@ -0,0 +1,46 @@
+import boto3
+import os
+
+
+def lambda_handler(event, context):
+    print(event)
+    #eventName = event.get(detail).get(eventName)
+    
+    try:
+        vpc_id = event['responseElements']['vpc']['vpcId']
+
+        print('VPC: ' + vpc_id)
+
+        ec2 = boto3.client('ec2')
+
+        response = ec2.describe_flow_logs(
+            Filter=[
+                {
+                    'Name': 'resource-id',
+                    'Values': [
+                        vpc_id,
+                    ]
+                },
+            ],
+        )
+
+        if len(response[u'FlowLogs']) != 0:
+            print('VPC Flow Logs are ENABLED')
+        else:
+            print('VPC Flow Logs are DISABLED')
+
+            print('FLOWLOGS_GROUP_NAME: ' + os.environ['FLOWLOGS_GROUP_NAME'])
+            print('ROLE_ARN: ' + os.environ['ROLE_ARN'])
+
+            response = ec2.create_flow_logs(
+                ResourceIds=[vpc_id],
+                ResourceType='VPC',
+                TrafficType='ALL',
+                LogGroupName=os.environ['FLOWLOGS_GROUP_NAME'],
+                DeliverLogsPermissionArn=os.environ['ROLE_ARN'],
+            )
+
+            print('Created Flow Logs: ' + response['FlowLogIds'][0])
+
+    except Exception as e:
+        print('Error - reason "%s"' % str(e))
